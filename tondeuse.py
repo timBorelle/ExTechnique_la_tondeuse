7cd1106c0ef7024c01fc65c548f059213aa98b08
timBorelle <tim-borelle@protonmail.com>
2021-06-02 15:00:38 +0200

    deleting some comments

--- tondeuse.py
+++ tondeuse.py
@@ -2,9 +2,7 @@ import re
 import numpy as np
 import logging
 
-
 def lireFichier(cheminSource):
-    #logging.info("lireFichier")
     fichierSource = open(cheminSource, 'r')
     global nbTondeuses, tondeuses, pelouse, nbCellsY
     nbTondeuses = 0
@@ -15,7 +13,6 @@ def lireFichier(cheminSource):
         # Nouvelle ligne depuis le fichier
         ligne = fichierSource.readline()
         refTondeuse = None
-        #logging.info(ligne)
         if not ligne:
             break
         
@@ -24,40 +21,29 @@ def lireFichier(cheminSource):
             nbCellsX = int(re.findall("\d+", ligne)[0]) + 1
             nbCellsY = int(re.findall("\d+", ligne)[1]) + 1
             pelouse = np.empty((nbCellsY,nbCellsX), list)
-            #logging.info("Taille de la pelouse: (%d, %d)", nbCellsX, nbCellsY)
         
         # Position d'une tondeuse
         if re.search("\d.*\d.*(N|E|W|S)", ligne):
             nbTondeuses += 1
             x = int(re.findall("\d+", ligne)[0])
             y = int(re.findall("\d+", ligne)[1])
-            #orientationActuelle= re.findall("\s+(N|S|E|O){1}\s+", ligne)
-            #orientationActuelle= re.findall("(N|E|W|S)+", ligne)
             orientationActuelle= ''.join(re.findall("\s+(N|S|E|O){1}\s+", ligne))
-            #logging.info("orientationActuelle: %s", str(orientationActuelle))
             infoTondeuse = []
             refTondeuse = "t" + str(nbTondeuses-1)
             infoTondeuse.append(refTondeuse)
             infoTondeuse.append(x)
             infoTondeuse.append(y)
             infoTondeuse.append(orientationActuelle)
-            #logging.info("nouvelle tondeuse: %s", str(infoTondeuse))
             
             positionY = nbCellsY - 1 - y
             pelouse[positionY][x] = refTondeuse
-            #logging.info("tondeuse #%s", pelouse[positionY][x])
             tondeuses.append(infoTondeuse)
-            #logging.info("tondeuses : %s", str(tondeuses))
 
         # Mouvements d'une tondeuse
         if re.search("^(A|D|G)+$", ligne):
             mouvements = re.findall("[A-G]{1}", ligne)        # [A-G]+
-            #logging.info("Mouvements: %s", str(mouvements))
-            #logging.info("tondeuse iNfO + mouv : ")
             indiceTondeuse = nbTondeuses-1
             tondeuses[indiceTondeuse].append(mouvements)
-            #tondeuses.insert(nbTondeuses-1, mouvements)
-            #logging.info(tondeuses)
     fichierSource.close()
 
 
@@ -111,8 +97,6 @@ def obtenirNouveauxCoordonnes(orientation, y, x):
 def estAlInterieur(y=int, x=int):
     hauteurPelouse = pelouse.shape[0]
     largeurPelouse = pelouse.shape[1]
-    #logging.info("hauteurPelouse : %d", hauteurPelouse)
-    #logging.info("largeurPelouse : %d", largeurPelouse)
     if y < hauteurPelouse and y >= 0 and x < largeurPelouse and x >= 0:
         return True
     else:
@@ -128,7 +112,6 @@ def emplacementDispo(y, x):
 
 
 def simulation():
-    logging.info("Simulation...")
     for i in range(nbTondeuses):
         infoTondeuse = tondeuses[i]
         refTondeuse = infoTondeuse[0]
@@ -140,29 +123,15 @@ def simulation():
 
         while prochainMouvement:
             x = infoTondeuse[1]
-            y = infoTondeuse[2]     #nbCellsY - 1 - infoTondeuse[2]      
+            y = infoTondeuse[2]          
             orientationActuelle = infoTondeuse[3]
-            #logging.info("x: %d", x)
-            #logging.info("y: %d", y)
 
-            #print("prochain mouv ###")
-            #print(prochainMouvement)
-            #logging.info("orientationActuelle : {%s}", orientationActuelle)
             nouvelleOrientation = obtenirNouvelleOrientation(orientationActuelle,prochainMouvement)
-            #logging.info("nouvelleOrientation : {%s}", str(nouvelleOrientation))
-            '''if nouvelleOrientation != orientationActuelle:
-                print(end="")
-                #mouvements = mouvements[1:]
-                # Mise à jour des informations sur la tondeuse
-                #tondeuses[i] = [refTondeuse, x, y, nouvelleOrientation, mouvements]
-            else:'''
             if nouvelleOrientation == orientationActuelle:
                 nouveauCoordY, nouveauCoordX = obtenirNouveauxCoordonnes(orientationActuelle, y, x)
                 # Vérifie si le mouvement est en dehors de la pelouse
                 if estAlInterieur(nouveauCoordY, nouveauCoordX):        
                     # Vérfie si l'emplacement est disponible
-                    #logging.info("y: %d", nouveauCoordY)
-                    #logging.info("x: %d", nouveauCoordX)
                     if emplacementDispo(nouveauCoordY, nouveauCoordX):
                         # Libération de l'emplacement précédent
                         positionY = nbCellsY - 1 - y
@@ -170,32 +139,24 @@ def simulation():
                         # Intégration d'un nouvel emplacement
                         positionY = nbCellsY - 1 - nouveauCoordY
                         pelouse[positionY][nouveauCoordX] = refTondeuse
-                        y = nouveauCoordY    #positionY
+                        y = nouveauCoordY    
                         x = nouveauCoordX
             # Suppression du mouvement effectué
             mouvements = mouvements[1:]
             # Mise à jour des informations sur la tondeuse
             infoTondeuse = [refTondeuse, x, y, nouvelleOrientation, mouvements]
-            #tondeuses[i] = [refTondeuse, x, y, nouvelleOrientation, mouvements]
             if mouvements:
                 prochainMouvement = mouvements[0]
             else:
                 prochainMouvement = None
-            #logging.info("infoTondeuse : ")
-            logging.info(infoTondeuse)
         tondeuses[i] = infoTondeuse
-        #logging.info("----- %s a effectué tous ses mouvements. -----", refTondeuse)
         print(x, y, orientationActuelle)
-        
 
-    
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    cheminSource = "./ressources/input2.txt"
+    cheminSource = "./ressources/input.txt"
     lireFichier(cheminSource)
-    afficherPelouse()
+    #afficherPelouse()
     simulation()
-    #logging.info("nouvelle pelouse :")
-    afficherPelouse()
-    #logging.info(tondeuses)
+    #afficherPelouse()
