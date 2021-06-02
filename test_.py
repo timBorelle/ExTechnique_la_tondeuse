a5c3e534adfcbc81a69c56e605a60234a2317cfe
timBorelle <tim-borelle@protonmail.com>
2021-06-02 13:13:30 +0200

    add test (pytest)

---
+++ test_.py
@@ -0,0 +1,18 @@
+import pytest
+import os
+
+global fichierSrc
+fichierSrc = "./ressources/input.txt"
+
+def isEmptyFile(fichierSrc):
+    if os.path.getsize(fichierSrc) == 0:
+        return True
+    return False
+
+# vérifie si le fichier existe
+def test_file_exist():
+    assert os.path.isfile(fichierSrc) == True
+
+# Vérifie si le fichier est vide
+def test_no_empty_file():
+    assert isEmptyFile(fichierSrc) == False
